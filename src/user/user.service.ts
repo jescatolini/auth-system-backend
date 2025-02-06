5c0cc373b7d48a8ff8c4e80329f5155acfcb40a9
Jhonata Escatolini <jescatolini@gmail.com>
2025-02-05 21:16:35 -0300

    service for create a new user

---
+++ src/user/user.service.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
+import { User, UserDocument } from './schemas/user.schema';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(username: string, password: string): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new this.userModel({ username, password: hashedPassword });
+    return newUser.save();
+  }
+
+  async findOne(username: string): Promise<User | null> {
+    return this.userModel.findOne({ username });
+  }
+}

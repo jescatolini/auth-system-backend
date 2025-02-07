7d6911742eb6a78f88f5e389cac04667042ab042
Jhonata Escatolini <jescatolini@gmail.com>
2025-02-07 11:23:10 -0300

    add Controllers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { UserService } from '../user/user.service';
 import { JwtModule } from '@nestjs/jwt';
 import { User, UserSchema } from '../user/schemas/user.schema';
 import { ConfigModule } from '@nestjs/config';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot(), // Importando o ConfigModule para carregar variáveis de ambiente
   ],
   providers: [AuthService, UserService],
+  controllers: [AuthController],
   exports: [AuthService],
 })
 export class AuthModule {}

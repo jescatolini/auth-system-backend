4b97177a36939514d9bae9137c377aff1da46de6
Jhonata Escatolini <jescatolini@gmail.com>
2025-02-07 01:23:36 -0300

    add email

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -9,13 +8,13 @@ import { User, UserDocument } from './schemas/user.schema';
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  async create(username: string, password: string): Promise<User> {
+  async create(username: string, password: string, email: string): Promise<User> {
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new this.userModel({ username, password: hashedPassword });
+    const newUser = new this.userModel({ username, password: hashedPassword, email });
     return newUser.save();
   }
 
-  async findOne(username: string): Promise<User | null> {
+  async findOne(username: string): Promise<UserDocument | null> { // Modificado para retornar UserDocument
     return this.userModel.findOne({ username });
   }
 }

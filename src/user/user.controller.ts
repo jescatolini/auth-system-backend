efdebea8dcd0f195aedbb16fd39c71872394bbfa
Jhonata Escatolini <jescatolini@gmail.com>
2025-02-07 01:23:50 -0300

    add email

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,13 @@
-/* eslint-disable prettier/prettier */
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('auth')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Post('register')
-  async register(@Body() body: { username: string; password: string }) {
-    const user = await this.userService.create(body.username, body.password);
+  async register(@Body() body: { username: string; password: string; email: string }) {
+    const user = await this.userService.create(body.username, body.password, body.email);
     return { message: 'User created successfully', user };
   }
 }
